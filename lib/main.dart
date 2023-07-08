4be06e4578fe91e98fca28bdae3444e6b3d31ce9
Mrunal333 <mrunalsheramkar333@2gmail.com>
2023-07-08 13:12:36 +0530

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'check_update.dart';
 
-
-
 void main() async {
   
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp( const CheckUpdate());
-  
+
 }
